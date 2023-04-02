ecdab2f2f08007d755a33772c577a78589a95b50
Psychpsyo <psychpsyo@gmail.com>
2023-04-02 05:59:55 +0200

    Switch to active twemoji fork

--- src/emoji.py
+++ src/emoji.py
@@ -1,19 +1,19 @@
 import requests
 
-# This uses Github's tree API to get the list of all twemoji in twitter/twemoji/assets/72x72.
+# This uses Github's tree API to get the list of all twemoji in jdecked/twemoji/assets/72x72.
 # It is necessary since the simple API call for git directory contents clamps the resulting list to 1000 entries which is not enough to get all emoji.
 # there is probably a better, more explicit way to write this but I do not care right now.
 
-_emojiList = requests.get("https://api.github.com/repos/twitter/twemoji/git/trees/" +
+_emojiList = requests.get("https://api.github.com/repos/jdecked/twemoji/git/trees/" +
 	next(
 		(tree for tree in
 			requests.get(
-				"https://api.github.com/repos/twitter/twemoji/git/trees/" + 
+				"https://api.github.com/repos/jdecked/twemoji/git/trees/" + 
 				next(
 					(tree for tree in
 						requests.get(
-							"https://api.github.com/repos/twitter/twemoji/git/trees/" + 
-							requests.get("https://api.github.com/repos/twitter/twemoji/branches/master").json()["commit"]["sha"]
+							"https://api.github.com/repos/jdecked/twemoji/git/trees/" + 
+							requests.get("https://api.github.com/repos/jdecked/twemoji/branches/master").json()["commit"]["sha"]
 						).json()["tree"]
 					if tree["path"] == "assets"),
 					None
